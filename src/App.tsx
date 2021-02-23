391f1350227b247ba632be258f5f253ae0a4ce35
unknown <danielvlima98@gmail.com>
2021-02-23 02:03:54 -0300

    0

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Button } from './components/Buttons'
+function App() {
+  return (
+
+    <Button />
+  
+  );
+}
+
+export default App;
---
+++ src/components/Buttons.tsx
@@ -0,0 +1,7 @@
+export function Button() {
+    return (
+        <button type="button">
+            cu
+        </button>
+    );
+}
