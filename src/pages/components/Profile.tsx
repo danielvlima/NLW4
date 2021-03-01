cfd090c205c1c9eb48869bcdf147e3a6a803bf56
unknown <danielvlima98@gmail.com>
2021-02-28 23:11:03 -0300

    versão 1.0

---
+++ src/pages/components/Profile.tsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengeContext';
+import styles from '../styles/components/Profile.module.css'
+export function Profile() {
+    const { level } = useContext(ChallengesContext)
+
+    return(
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/danielvlima.png" alt="Daniel"/>
+        
+            <div>
+                <strong>daniel lima</strong>
+                <p>
+                    <img src="icons/level.svg" alt="level"/>
+                    Level {level}</p>
+            </div>
+        </div>
+    );
+}
